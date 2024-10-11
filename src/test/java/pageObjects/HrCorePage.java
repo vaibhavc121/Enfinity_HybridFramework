35dd3e0d4e285c5f7dc10443d3e16dbbb61dc2c1
vaibhav <vaibhavc121@gmail.com>
2024-10-11 11:23:04 +0530

    1

--- src/test/java/pageObjects/HrCorePage.java
+++ src/test/java/pageObjects/HrCorePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-class HRCorePage extends BasePage
+public class HRCorePage extends BasePage
 {
 
 	public HRCorePage(WebDriver driver)
