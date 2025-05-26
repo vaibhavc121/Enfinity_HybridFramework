1a1dfc96dc0fe22708087eb7b95a95c1793bcd24
vaibhavc121 <vaibhavc121@gmail.com>
2025-05-26 18:55:36 +0530

    26-5

--- src/test/java/pageObjects/HRMS/Payroll/LeaveAdjustmentPage.java
+++ src/test/java/pageObjects/HRMS/Payroll/LeaveAdjustmentPage.java
@@ -9,118 +9,106 @@ import base.BasePage;
 public class LeaveAdjustmentPage extends BasePage
 {
 
-	public LeaveAdjustmentPage(WebDriver driver) 
+	public LeaveAdjustmentPage(WebDriver driver)
 	{
 		super(driver);
 		// TODO Auto-generated constructor stub
 	}
-	
-	@FindBy(xpath="//span[normalize-space()='Leave Adjustment']") WebElement leaveAdjustment;
-	
-	@FindBy(xpath="//span[normalize-space()='New']") WebElement newbtn;
-	
-	@FindBy(css="img[id='LeaveAdjustment.EmployeeIdLookup_B-1Img']") WebElement empdd;
-	
-	@FindBy(xpath="//div[contains(text(),'001 | Vaibhav Chavan')]") WebElement emp;
-	//public String emp1=emp.getText();
-	
-	@FindBy(css="img[id='LeaveAdjustment.LeaveTypeIdLookup_B-1Img']") WebElement leavetypedd;
-	
-	@FindBy(xpath="//div[contains(text(),'Sick Leave')]") WebElement sickLeave;
-	
-	@FindBy(xpath="//input[@id='LeaveAdjustment.PaidDays_I']") WebElement paidDaysTB;
-	
-	@FindBy(xpath="//input[@id='LeaveAdjustment.UnpaidDays_I']") WebElement unpaidDaysTB;
-	
-	@FindBy(xpath="//textarea[@id='LeaveAdjustment.Description_I']") WebElement remarks;
-	
-	@FindBy(xpath="//span[normalize-space()='View']") WebElement view;
-	
-	@FindBy(xpath="//span[normalize-space()='Approve']") WebElement approve;
-	
-	@FindBy(xpath="/html[1]/body[1]/div[6]/div[2]/div[1]/div[1]/div[1]/div[5]/div[1]/table[1]/tbody[1]/tr[2]/td[6]/div[1]/div[2]/div[1]/div[1]/div[1]/input[1]") WebElement filterCell;;
-	
-	@FindBy(xpath="/html[1]/body[1]/div[6]/div[2]/div[1]/div[1]/div[1]/div[6]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[6]/span[1]/a[1]") WebElement result;;
-	
-	
+
+	@FindBy(xpath = "//span[normalize-space()='Leave Adjustment']")
+	WebElement leaveAdjustment;
+
+	@FindBy(css = "img[id='LeaveAdjustment.EmployeeIdLookup_B-1Img']")
+	WebElement empdd;
+
+	@FindBy(xpath = "//input[@id='LeaveAdjustment.EffectiveDate_I']")
+	WebElement effectiveDate;
+
+	@FindBy(css = "img[id='LeaveAdjustment.LeaveTypeIdLookup_B-1Img']")
+	WebElement leavetypedd;
+
+	@FindBy(xpath = "//input[@id='LeaveAdjustment.PaidDays_I']")
+	WebElement paidDaysTB;
+
+	@FindBy(xpath = "//input[@id='LeaveAdjustment.UnpaidDays_I']")
+	WebElement unpaidDaysTB;
+
+	@FindBy(xpath = "//textarea[@id='LeaveAdjustment.Description_I']")
+	WebElement remarks;
+
+	@FindBy(xpath = "//span[normalize-space()='Approve']")
+	WebElement approve;
+
+	@FindBy(xpath = "/html[1]/body[1]/div[6]/div[2]/div[1]/div[1]/div[1]/div[5]/div[1]/table[1]/tbody[1]/tr[2]/td[6]/div[1]/div[2]/div[1]/div[1]/div[1]/input[1]")
+	WebElement filterCell;;
+
+	@FindBy(xpath = "/html[1]/body[1]/div[6]/div[2]/div[1]/div[1]/div[1]/div[6]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[6]/span[1]/a[1]")
+	WebElement result;;
+
 	public void clkLeaveAdj()
 	{
 		leaveAdjustment.click();
 	}
-	
+
 	public void clkNewBtn()
 	{
-		newbtn.click();
+		clickOnNew();
 	}
-	
-	public void clkEmpDD()
+
+	public void provideEmp(String value)
 	{
-		empdd.click();
+		clearAndProvide1(empdd, value);
 	}
-	
-	public void slctEmp() throws InterruptedException
+
+	public void provideEffectiveDate(String value)
 	{
-		emp.click();
-		Thread.sleep(2000);
+		clearAndProvide1(effectiveDate, value);
 	}
-	
-	public void clkLeaveTypeDD()
+
+	public void provideLeaveType(String value)
 	{
-		leavetypedd.click();
+		clearAndProvide1(leavetypedd, value);
 	}
-	
-	public void slctLeaveType()
+
+	public void providePaidDaysValue(String value)
 	{
-		sickLeave.click();
+		clearAndProvide1(paidDaysTB, value);
 	}
-	
-	public void providePaidDaysValue()
+
+	public void provideUnpaidDaysValue(String value)
 	{
-		paidDaysTB.clear();
-		paidDaysTB.sendKeys("1");
+		clearAndProvide1(unpaidDaysTB, value);
 	}
-	
-	public void provideUnpaidDaysValue()
-	{
-		unpaidDaysTB.clear();
-		unpaidDaysTB.sendKeys("1");
-	}
-	
+
 	public void provideRemarks()
 	{
 		remarks.sendKeys("remarks");
 	}
-	
+
 	public void clkViewBtn()
 	{
-		view.click();
+		clickOnView();
 	}
-	
+
 	public void clkApproveBtn() throws InterruptedException
-	{		
-		approve.click();
-		driver.navigate().back();
-		Thread.sleep(2000);
+	{
+		clickOnApprove();
 	}
-	
+
 	public boolean isTxnCreated()
 	{
-		//String expemp=emp.getText();
-		String expemp="Vaibhav Chavan";
+		// String expemp=emp.getText();
+		String expemp = "Vaibhav Chavan";
 		filterCell.sendKeys(expemp);
-		String actemp=result.getText();
-		
-		if(actemp.contains(expemp))
+		String actemp = result.getText();
+
+		if (actemp.contains(expemp))
 		{
 			return true;
-		}
-		else
+		} else
 		{
 			return false;
 		}
 	}
-	
-	
-	
 
 }
