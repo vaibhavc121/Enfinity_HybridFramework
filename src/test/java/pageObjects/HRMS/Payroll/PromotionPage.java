e7b79cf8a49941c5d889acaf6e69080a2bd06310
vaibhavc121 <vaibhavc121@gmail.com>
2025-06-19 19:01:21 +0530

    19-6

--- src/test/java/pageObjects/HRMS/Payroll/PromotionPage.java
+++ src/test/java/pageObjects/HRMS/Payroll/PromotionPage.java
@@ -1,16 +1,69 @@
 package pageObjects.HRMS.Payroll;
 
+import base.SelenideBasePage;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.WebDriver;
 
 import base.BasePage;
 
-public class PromotionPage extends BasePage
-{
+import static com.codeborne.selenide.Selenide.$x;
 
-	public PromotionPage(WebDriver driver)
+public class PromotionPage extends SelenideBasePage
+{
+	//region Locators
+	SelenideElement promotion = $x("(//span[text()='Promotion'])[2]");
+	//endregion
+
+	//region Action Methods
+	public void clickPromotion()
 	{
-		super(driver);
-		
+		promotion.click();
 	}
+
+	public void clickNew()
+	{
+		clickOnNew();
+	}
+
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+//
+//	public void a()
+//	{
+//	}
+	//endregion
 
 }
