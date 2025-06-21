850ed597f3b8e3653b2325ec001664ae853c0cce
vaibhav <vaibhavc121@gmail.com>
2025-06-21 11:35:47 +0530

    21-6

--- src/test/java/testCases/HRMS/SelfService/DeleteLeaveRequestTest.java
+++ src/test/java/testCases/HRMS/SelfService/DeleteLeaveRequestTest.java
@@ -11,32 +11,31 @@ import pageObjects.HRMS.SelfService.SelfServicePage;
 import utilities.FileUtils;
 import utilities.JsonUtils;
 import utilities.RetryAnalyzer;
+
 import java.util.List;
 
 public class DeleteLeaveRequestTest extends BaseTest
 {
-	@Test(groups = "regression", retryAnalyzer = RetryAnalyzer.class)
-	public void deleteLeaveRequest()
-	{
-		try
-		{
+    @Test(groups = "regression", retryAnalyzer = RetryAnalyzer.class)
+    public void deleteLeaveRequest()
+    {
+        try
+        {
 
-			// self service page
-			SelfServicePage ss = new SelfServicePage(driver);
-			ss.clickSelfService();
-			ss.clickTransactions();
+            // self service page
+            SelfServicePage ss = new SelfServicePage(driver);
+            ss.clickSelfService();
+            ss.clickTransactions();
 
-			// leave request page
-			LeaveRequestPage lr = new LeaveRequestPage(driver);
-			lr.clickLeaveRequest();
+            // leave request page
+            LeaveRequestPage lr = new LeaveRequestPage(driver);
+            lr.clickLeaveRequest();
 
-			BasePage.deleteTxn(7, "active");
-
-		} catch (Exception e)
-		{
-			logger.error("Test failed due to exception: ", e);
-			Assert.fail("Test case failed: " + e);
-		}
-	}
-
+            BasePage.deleteTxn(7, "active");
+        } catch (Exception e)
+        {
+            logger.error("Test failed due to exception: ", e);
+            Assert.fail("Test case failed: " + e);
+        }
+    }
 }
