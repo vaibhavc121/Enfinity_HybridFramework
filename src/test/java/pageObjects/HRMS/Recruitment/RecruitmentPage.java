ac0b9c030017267d38281e5ed46f983dfffc82ec
vaibhavc121 <vaibhavc121@gmail.com>
2025-08-11 18:20:06 +0530

    11-8

--- src/test/java/pageObjects/HRMS/Recruitment/RecruitmentPage.java
+++ src/test/java/pageObjects/HRMS/Recruitment/RecruitmentPage.java
@@ -10,48 +10,52 @@ import base.BasePage;
 public class RecruitmentPage extends BasePage
 {
 
-	public RecruitmentPage(WebDriver driver)
-	{
-		super(driver);
+    public RecruitmentPage(WebDriver driver)
+    {
+        super(driver);
+    }
 
-	}
+    @FindBy(xpath = "//span[normalize-space()='Recruitment']")
+    private WebElement recruitment;
 
+    @FindBy(xpath = "//a[@title='Job']//span[@class='dx-vam'][normalize-space()='Job']")
+    private WebElement job;
 
-	@FindBy(xpath = "//span[normalize-space()='Recruitment']")
-	private WebElement recruitment;
+    @FindBy(xpath = "//span[@class='dx-vam'][normalize-space()='Candidate']")
+    private WebElement candidate;
 
-	@FindBy(xpath = "//a[@title='Job']//span[@class='dx-vam'][normalize-space()='Job']")
-	private WebElement job;
+    public void clickOnHambergurIcon()
+    {
+        boolean value = waitForElement(job).isDisplayed();
+        if (!value)
+        {
+            clickOnHamburgerMenu();
+            BaseTest.log("Hamburger icon clicked");
+        } else
+        {
+            BaseTest.log("Job Name is already displayed, no need to click on Hamburger icon");
+        }
+    }
 
-	@FindBy(xpath="//span[@class='dx-vam'][normalize-space()='Candidate']") private WebElement candidate;
+    public void clickRecruitment()
+    {
+        waitTS(3);
+        try
+        {
+            clickOnElement1(recruitment);
+        } catch (Exception e)
+        {
+            clickOnElement1(recruitment);
+        }
+    }
 
-	public  void clickOnHambergurIcon()
-	{
-		boolean value=  waitForElement(job).isDisplayed();
-		if(!value)
-		{
-			clickOnHamburgerMenu();
-			BaseTest.log("Hamburger icon clicked");
-		}
-		else
-		{
-			BaseTest.log("Job Name is already displayed, no need to click on Hamburger icon");
-		}
-	}
+    public void clickJob()
+    {
+        job.click();
+    }
 
-	public void clickRecruitment()
-	{
-		recruitment.click();
-	}
-
-	public void clickJob()
-	{
-		job.click();
-	}
-
-	public void clickCandidate()
-	{
-		waitForElement(candidate).click();
-	}
-
+    public void clickCandidate()
+    {
+        waitForElement(candidate).click();
+    }
 }
